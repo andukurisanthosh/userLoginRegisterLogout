d4ef46129e01d66c9cfdf32c6edeecbeccef855c
Srinivas Andukuri <92281933+andukurisanthosh@users.noreply.github.com>
2023-03-30 19:46:41 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentsProfile.Models;
+
+namespace StudentsProfile.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentsProfile.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
